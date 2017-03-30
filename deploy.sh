a13a13b716253f4671dae0573363393424c2282f
Ivan Plyusnin <ivan.plyusnin@gmail.com>
2017-03-30 15:43:33 +0300

    Update server ip

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 # docker build -t kostyaurysov/sample-node .
 docker push ivanplus/sample-node
 
-ssh ivn_plus@104.155.22.214 << EOF
+ssh ivn.plus@35.187.100.237  << EOF
 docker pull ivanplus/sample-node:latest
 docker stop web || true
 docker rm web || true
 docker rmi ivanplus/sample-node:current || true
 docker tag ivanplus/sample-node:latest ivanplus/sample-node:current
-docker run -d --net lab6 --restart always --name web -p 80:80 ivanplus/sample-node:current
+docker run -d --net app --restart always --name web -p 80:80 ivanplus/sample-node:current
 EOF
